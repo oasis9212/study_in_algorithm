d83e1ee756c4780fe67b4c4e8bfab8d0a55e99b4
wjs <oasis9212@gmail.com>
2025-01-04 12:39:16 +0900

    배포

--- stu_20241228/leetcode_1829.java
+++ stu_20241228/leetcode_1829.java
@@ -11,10 +11,10 @@ public class leetcode_1829 {
 
     private static int[] sol_1829(int[] nums, int maximumBit) {
 
-        int maxbit = (int) Math.pow(2, maximumBit) - 1;
+        int maxbit = (int) Math.pow(2, maximumBit) - 1;  // 비트갯수 111 은 2^3 -1 값
         int[] result = new int[nums.length];
         int xor = 0;
-        for (int i = 0; i < nums.length; i++) {
+        for (int i = 0; i < nums.length; i++) {  // 배열의 길이가 커서 역순으로 진행함. 다이나믹
             xor = (xor ^ nums[i]);
             result[nums.length - i - 1] = xor ^ maxbit;
         }
